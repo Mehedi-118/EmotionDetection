41070ad7a4a0b803d28deaa01ed9d74d31e6692b
hasan.sami <hasan.sami@egeneration.co>
2022-12-22 11:42:48 +0600

    Dataset uploaded and cleaned

--- test.py
+++ test.py
@@ -1,1 +1,120 @@
-print("checking")
+import pandas as pd
+import numpy as np
+
+# Visualization libraries
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Text Libraries
+import nltk
+import nltk
+nltk.download('omw-1.4')
+
+import string
+import re
+
+# Feature Extraction Libraries
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split
+
+# Classifier Model libraries
+from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+# from xgboost import XGBClassifier
+# import xgboost as xgb
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import tree
+from sklearn.neural_network import MLPClassifier
+
+# Performance Matrix libraries
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import ConfusionMatrixDisplay
+
+# other
+import pickle
+import os
+import warnings
+
+warnings.filterwarnings("ignore")
+# printing the frequency of Sentiment
+import collections
+
+#:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+#                                                   Data Read From CSV
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+df_emotion = pd.read_csv('text_emotions_neutral.csv')
+
+print('Dataset size:', df_emotion.shape)
+print('Columns are:', df_emotion.columns)
+
+# Visualize the Sentiments
+sns.countplot(x='sentiment', data=df_emotion)
+
+# plt.show()
+emotion_counter = collections.Counter(df_emotion['sentiment'])
+emotion_counter
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#                                                   Text Processing
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# Remove Punctuation, Replace emojis
+emojis = pd.read_csv('emojis.txt',sep=',',header=None)
+emojis_dict = {i:j for i,j in zip(emojis[0],emojis[1])}
+pattern = '|'.join(sorted(re.escape(k) for k in emojis_dict))
+
+def replace_emojis(text):
+    text = re.sub(pattern,lambda m: emojis_dict.get(m.group(0)), text, flags=re.IGNORECASE)
+    return text
+
+def remove_punct(text):
+    text = replace_emojis(text)
+    text = "".join([char for char in text if char not in string.punctuation])
+    text = re.sub('[0-9]+', '', text)
+    return text
+
+# Tokenization
+def tokenization(text):
+    text = text.lower()
+    text = re.split('\W+', text)
+    return text
+
+# Remove Stopwords
+nltk.download('stopwords')
+stopword = nltk.corpus.stopwords.words('english')
+stopword.extend(['yr', 'year', 'woman', 'man', 'girl','boy','one',  'two', 'sixteen', 'yearold', 'fu', 'weeks', 'week', 'treatment', 'associated', 'patients', 'may','day', 'case','old','u','n','didnt','ive','ate','feel','keep','brother','dad','basic','im',''])
+
+def remove_stopwords(text):
+    text = [word for word in text if word not in stopword]
+    return text
+
+# Lemmatization
+nltk.download('wordnet')
+wn = nltk.WordNetLemmatizer()
+
+def lemmatizer(text):
+    text = [wn.lemmatize(word) for word in text]
+    return text
+
+def clean_text(text):
+    text = remove_punct(text)
+    text = tokenization(text)
+    text = remove_stopwords(text)
+    text = lemmatizer(text)
+    return text
+
+# Dataset Splitting in 70%-30%
+X_train, X_test, y_train, y_test = train_test_split(df_emotion['content'], df_emotion['sentiment'],test_size=0.3, random_state =116)
+countVectorizer1 = CountVectorizer(analyzer=clean_text)
+countVector1 = countVectorizer1.fit_transform(X_train)
+print(X_train)
